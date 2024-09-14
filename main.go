0d9869b86ad57eaa6c0518fd3235e3a9e6e7b68a
hezijie <lonegunmanb@hotmail.com>
2024-09-14 14:10:45 +0800

    only purge residual resource group since we don't have any 1es runner pool now

--- main.go
+++ main.go
@@ -4,15 +4,12 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"regexp"
 	"strconv"
 	"sync"
 	"time"
 
 	"github.com/lonegunmanb/oneesrunnerscleaner/pkg"
 )
-
-var runnerNameRegex = regexp.MustCompile("[a-z0-9_]{15}")
 
 func main() {
 	subscriptionId := readEssentialEnv("AZURE_SUBSCRIPTION_ID")
@@ -22,7 +19,6 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	purgeRunnerPools(err, client)
 	purgeResidualResourceGroups(client)
 }
 
@@ -36,6 +32,20 @@ func purgeResidualResourceGroups(client *pkg.Client) {
 		panic(err.Error())
 	}
 	wg := sync.WaitGroup{}
+
+	existingRgs := make(map[string]struct{})
+	for _, rg := range groups {
+		existingRgs[rg.Name] = struct{}{}
+	}
+	var deprecatedRgs []string
+	for k, _ := range recordRg.Tags {
+		if _, ok := existingRgs[k]; !ok {
+			deprecatedRgs = append(deprecatedRgs, k)
+		}
+	}
+	for _, rg := range deprecatedRgs {
+		delete(recordRg.Tags, rg)
+	}
 
 	for _, rg := range groups {
 		if rg.IsProtected() {
@@ -65,49 +75,6 @@ func purgeResidualResourceGroups(client *pkg.Client) {
 		panic(err.Error())
 	}
 	wg.Wait()
-}
-
-func purgeRunnerPools(err error, client *pkg.Client) {
-	pools, err := client.ListPools()
-	if err != nil {
-		panic(fmt.Sprintf("cannot list pools: %+v", err))
-	}
-	fmt.Printf("List pools, got %d pools\n", len(pools.Data))
-	for _, pool := range pools.Data {
-		var runnerNames []string
-		fmt.Printf("Get runners for %s/%s\n", pool.ResourceGroup, pool.Name)
-		runners, err := client.GetRunners(pool.ResourceGroup, pool.Name)
-		if err != nil {
-			println(fmt.Sprintf("cannot get runners for %s/%s: %+v", pool.ResourceGroup, pool.Name, err))
-			continue
-		}
-		for _, r := range runners {
-			if r.Status != "Allocated" {
-				continue
-			}
-			if _, seen := pool.Tags[r.Name]; !seen {
-				fmt.Printf("  unseen %s\n", r.Name)
-				runnerNames = append(runnerNames, r.Name)
-				continue
-			}
-			fmt.Printf("  purge runner %s from %s/%s\n", r.Id, pool.ResourceGroup, pool.Name)
-			err = client.PurgeRunner(pool.ResourceGroup, pool.Name, r.Id)
-			if err != nil {
-				runnerNames = append(runnerNames, r.Name)
-			}
-		}
-		tags := make(map[string]any)
-		for key, value := range pool.Tags {
-			if !runnerNameRegex.Match([]byte(key)) {
-				tags[key] = value
-			}
-		}
-		for _, n := range runnerNames {
-			tags[n] = time.Now().UTC().String()
-		}
-		fmt.Printf("upgrade tags for %s/%s\n", pool.ResourceGroup, pool.Name)
-		_ = client.UpgradePoolTags(pool, tags)
-	}
 }
 
 func readEssentialEnv(envName string) string {
--- pkg/residual_resource_group_cleaner.go
+++ pkg/residual_resource_group_cleaner.go
@@ -1,9 +1,13 @@
 package pkg
 
 import (
+	"context"
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resourcegraph/armresourcegraph"
 	"strings"
 )
 
@@ -19,12 +23,11 @@ type ResourceGroup struct {
 }
 
 var protectedNameHashes = map[string]struct{}{
-	"403c2d5ed4f3bd5390c33e9a0b38e2ff": {},
-	"45359b60f526c42dc5f169b79183a782": {},
 	"7e8e0b947214e31d9f02a94090487c0d": {},
 	"cdb5195b6e59c8d4743d60fe5d1ddd54": {},
 	"f054bc18556dfd9d4cd5c6f92a3b96db": {},
 	"e35386fdf3abd880dd3b57c7bcb2340f": {},
+	"62e5e9c30b987e3d11dbeb7a1b07ff70": {},
 }
 
 func (rg ResourceGroup) IsProtected() bool {
@@ -112,4 +115,40 @@ func (c *Client) DeleteResourceGroup(name string) error {
 		return fmt.Errorf("cannot delete resource group %s: %+v", name, err)
 	}
 	return nil
+}
+
+type Client struct {
+	SubscriptionId string
+	TenantId       string
+	resourceClient *ResourceClient
+	ctx            context.Context
+	graphClient    *armresourcegraph.Client
+}
+
+func NewClient(subscriptionId, tenantId string, ctx context.Context) (*Client, error) {
+	cred, err := azidentity.NewDefaultAzureCredential(&azidentity.DefaultAzureCredentialOptions{
+		TenantID: tenantId,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("cannot create credential: %+v", err)
+	}
+
+	resCli, err := NewResourceClient(cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create resource client: %+v", err)
+	}
+	graphCli, err := armresourcegraph.NewClient(cred, &arm.ClientOptions{
+		DisableRPRegistration: true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("cannot create graph client: %+v", err)
+	}
+	c := &Client{
+		SubscriptionId: subscriptionId,
+		TenantId:       tenantId,
+		resourceClient: resCli,
+		graphClient:    graphCli,
+		ctx:            ctx,
+	}
+	return c, nil
 }
